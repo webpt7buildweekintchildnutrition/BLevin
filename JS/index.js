5e6efb623906b7ff92c5bcfd7827c166544fbedc
Brian <lawmen218@yahoo.com>
2019-06-28 23:46:47 -0400

    attempted JavaScript for donate page

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+
 const startButton = document.querySelector('.start');
 const startLink = document.querySelector('a');
 
@@ -8,3 +9,23 @@ startButton.addEventListener('click', (event) => {
     event.preventDefault();
 });
 
+
+
+/*Donate Page*/
+
+const button = document.querySelector(".donate-button");
+// const img = document.querySelector("img");
+// const clickHandler = function () {
+//     this.classList.toggle("btn-class");
+// }
+
+button.addEventListener("click", (event) => {
+    event.stopPropagation()
+    button.classList.toggle("btn-class");
+    // img.style.display = "none";
+    if (button.textContent === "Donate Here") {
+        button.textContent = "Thank you, you beautiful human being"
+    } else {
+        button.textContent = "Donate Here"
+    }
+});
